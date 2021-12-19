4c2c8480c8d06fbe3b7cf8e5f9fb4b5a844508aa
Mert Kaan YILMAZ <e238109@metu.edu.tr>
2021-12-19 11:19:41 +0300

    hw2 initials

---
+++ CENG-477/HW-2/src/Camera.h
@@ -0,0 +1,43 @@
+#ifndef __CAMERA_H__
+#define __CAMERA_H__
+
+#include "Vec3.h"
+#include <string>
+
+using namespace std;
+
+class Camera
+{
+
+public:
+    int cameraId;
+    int projectionType; // 1 for perspective, 0 for orthographic
+    Vec3 pos;
+    Vec3 gaze;
+    Vec3 u;
+    Vec3 v;
+    Vec3 w;
+    double left, right, bottom, top;
+    double near;
+    double far;
+    int horRes;
+    int verRes;
+    string outputFileName;
+
+    Camera();
+
+    Camera(int cameraId,
+           int projectionType,
+           Vec3 pos, Vec3 gaze,
+           Vec3 u, Vec3 v, Vec3 w,
+           double left, double right, double bottom, double top,
+           double near, double far,
+           int horRes, int verRes,
+           string outputFileName);
+
+    Camera(const Camera &other);
+
+    friend std::ostream &operator<<(std::ostream &os, const Camera &c);
+};
+
+#endif
