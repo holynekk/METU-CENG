4f9cbd6abf362164bebbf5175b840627cf6f67d3
holynekk <e238109@metu.edu.tr>
2023-05-15 20:57:48 +0300

    matrix additions.

--- CENG-334/THE-II/main.cpp
+++ CENG-334/THE-II/main.cpp
@@ -1,15 +1,34 @@
 #include <pthread.h>
 #include <iostream>
+#include <mutex>
 
 #include "hw2_output.h"
 
-// void *worker_thread(void *arg)
-// {
-//     printf("This is worker_thread()\n");
-//     pthread_exit(NULL);
-// }
-
 using namespace std;
+
+mutex mtx;
+
+typedef struct
+{
+    int matrix_id;
+    int **matrix1;
+    int **matrix2;
+    int **sum_matrix;
+    int col_num;
+    int row;
+} sum_args;
+
+void *sum_thread(void *arg)
+{
+    sum_args *args = (sum_args *)arg;
+
+    for (int i = 0; i < args->col_num; i++)
+    {
+        int result = args->matrix1[args->row][i] + args->matrix2[args->row][i];
+        args->sum_matrix[args->row][i] = result;
+        hw2_write_output(args->matrix_id, args->row + 1, i + 1, result);
+    }
+}
 
 int main(void)
 {
@@ -19,6 +38,8 @@ int main(void)
 
     // First matrix input ---------------------------------------------
     cin >> m >> n;
+    int first_matrix_rows = m;
+    int first_matrix_cols = n;
     int **first_matrix = new int *[m];
     for (int i = 0; i < m; ++i)
         first_matrix[i] = new int[n];
@@ -45,8 +66,14 @@ int main(void)
         }
     }
 
+    int **sum1_mat = new int *[m];
+    for (int i = 0; i < m; ++i)
+        sum1_mat[i] = new int[n];
+
     // Third matrix input ---------------------------------------------
     cin >> m >> k;
+    int third_matrix_rows = m;
+    int third_matrix_cols = k;
     int **third_matrix = new int *[m];
     for (int i = 0; i < m; ++i)
         third_matrix[i] = new int[k];
@@ -73,16 +100,69 @@ int main(void)
         }
     }
 
-    // pthread_t my_thread;
-    // int ret;
+    int **sum2_mat = new int *[m];
+    for (int i = 0; i < m; ++i)
+        sum2_mat[i] = new int[k];
 
-    // printf("In main: creating thread\n");
-    // ret = pthread_create(&my_thread, NULL, &worker_thread, NULL);
-    // if (ret != 0)
-    // {
-    //     printf("Error: pthread_create() failed\n");
-    //     exit(EXIT_FAILURE);
-    // }
+    int **result_mat = new int *[first_matrix_rows];
+    for (int i = 0; i < m; ++i)
+        result_mat[i] = new int[third_matrix_cols];
 
-    // pthread_exit(NULL);
+    pthread_t *sum1_threads = new pthread_t[first_matrix_rows];
+    pthread_t *sum2_threads = new pthread_t[third_matrix_rows];
+
+    for (int i = 0; i < first_matrix_rows; i++)
+    {
+        sum_args *thread_args = new sum_args;
+        thread_args->matrix_id = 0;
+        thread_args->matrix1 = first_matrix;
+        thread_args->matrix2 = second_matrix;
+        thread_args->sum_matrix = sum1_mat;
+        thread_args->col_num = first_matrix_cols;
+        thread_args->row = i;
+        pthread_create(sum1_threads + i, NULL, sum_thread, (void *)thread_args);
+    }
+
+    for (int i = 0; i < third_matrix_rows; i++)
+    {
+        sum_args *thread_args = new sum_args;
+        thread_args->matrix_id = 1;
+        thread_args->matrix1 = third_matrix;
+        thread_args->matrix2 = fourth_matrix;
+        thread_args->sum_matrix = sum2_mat;
+        thread_args->col_num = third_matrix_cols;
+        thread_args->row = i;
+        pthread_create(sum2_threads + i, NULL, sum_thread, (void *)thread_args);
+    }
+
+    // Join threads -------------------------------------------------------
+    for (int i = 0; i < first_matrix_rows; i++)
+    {
+        pthread_join(sum1_threads[i], NULL);
+    }
+
+    for (int i = 0; i < third_matrix_rows; i++)
+    {
+        pthread_join(sum2_threads[i], NULL);
+    }
+
+    for (int i = 0; i < first_matrix_rows; i++)
+    {
+        for (int j = 0; j < third_matrix_cols; j++)
+        {
+            for (int k = 0; k < third_matrix_rows; k++)
+            {
+                result_mat[i][j] += sum1_mat[i][k] * sum2_mat[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < first_matrix_rows; i++)
+    {
+        for (int j = 0; j < third_matrix_cols; j++)
+        {
+            cout << result_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
