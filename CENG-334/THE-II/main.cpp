4da567b64a9319a67c541681c229521d3d17e893
holynekk <e238109@metu.edu.tr>
2023-05-15 17:38:17 +0300

    input process

--- CENG-334/THE-II/main.cpp
+++ CENG-334/THE-II/main.cpp
@@ -1,25 +1,88 @@
 #include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-void *worker_thread(void *arg)
-{
-    printf("This is worker_thread()\n");
-    pthread_exit(NULL);
-}
+#include "hw2_output.h"
+
+// void *worker_thread(void *arg)
+// {
+//     printf("This is worker_thread()\n");
+//     pthread_exit(NULL);
+// }
+
+using namespace std;
 
 int main(void)
 {
-    pthread_t my_thread;
-    int ret;
+    hw2_init_output();
 
-    printf("In main: creating thread\n");
-    ret = pthread_create(&my_thread, NULL, &worker_thread, NULL);
-    if (ret != 0)
+    int n, m, k;
+
+    // First matrix input ---------------------------------------------
+    cin >> m >> n;
+    int **first_matrix = new int *[m];
+    for (int i = 0; i < m; ++i)
+        first_matrix[i] = new int[n];
+
+    for (int i = 0; i < m; i++)
     {
-        printf("Error: pthread_create() failed\n");
-        exit(EXIT_FAILURE);
+        for (int j = 0; j < n; j++)
+        {
+            cin >> first_matrix[i][j];
+        }
     }
 
-    pthread_exit(NULL);
+    // Second matrix input ---------------------------------------------
+    cin >> m >> n;
+    int **second_matrix = new int *[m];
+    for (int i = 0; i < m; ++i)
+        second_matrix[i] = new int[n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> second_matrix[i][j];
+        }
+    }
+
+    // Third matrix input ---------------------------------------------
+    cin >> m >> k;
+    int **third_matrix = new int *[m];
+    for (int i = 0; i < m; ++i)
+        third_matrix[i] = new int[k];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            cin >> third_matrix[i][j];
+        }
+    }
+
+    // Fourth matrix input ---------------------------------------------
+    cin >> m >> k;
+    int **fourth_matrix = new int *[m];
+    for (int i = 0; i < m; ++i)
+        fourth_matrix[i] = new int[k];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            cin >> fourth_matrix[i][j];
+        }
+    }
+
+    // pthread_t my_thread;
+    // int ret;
+
+    // printf("In main: creating thread\n");
+    // ret = pthread_create(&my_thread, NULL, &worker_thread, NULL);
+    // if (ret != 0)
+    // {
+    //     printf("Error: pthread_create() failed\n");
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // pthread_exit(NULL);
 }
